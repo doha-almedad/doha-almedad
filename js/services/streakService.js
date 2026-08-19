996060c42c6a46e40834bbaa31cf1276218fc74b
doha-almedad <ll4205858@gmail.com>
2026-08-19 13:41:42 +0300

    Update streakService.js

--- js/services/streakService.js
+++ js/services/streakService.js
@@ -1,1 +1,19 @@
 
+const StreakService = {
+    recordActivity() {
+        const data = Store.get();
+        const today = new Date().toISOString().split('T')[0];
+        const lastActive = localStorage.getItem('douha_last_active');
+
+        if (lastActive !== today) {
+            data.user.streak += 1;
+            localStorage.setItem('douha_last_active', today);
+            Store.save(data);
+        }
+    },
+
+    getStreak() {
+        const data = Store.get();
+        return data.user.streak || 0;
+    }
+};
