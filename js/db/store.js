2433e56377ba7c952bc94ea999d6a4a9ac48a966
doha-almedad <ll4205858@gmail.com>
2026-08-19 13:40:36 +0300

    Update store.js

--- js/db/store.js
+++ js/db/store.js
@@ -1,1 +1,25 @@
 
+const Store = {
+    init() {
+        if (!localStorage.getItem('douha_data')) {
+            localStorage.setItem('douha_data', JSON.stringify(InitialData));
+        }
+    },
+
+    get() {
+        this.init();
+        return JSON.parse(localStorage.getItem('douha_data'));
+    },
+
+    save(data) {
+        localStorage.setItem('douha_data', JSON.stringify(data));
+    },
+
+    updateUserStats(key, amount) {
+        const data = this.get();
+        if (data.user.stats[key] !== undefined) {
+            data.user.stats[key] += amount;
+            this.save(data);
+        }
+    }
+};
