18556925fa3c63f2622d58abd9f86c3a3a7b809e
doha-almedad <ll4205858@gmail.com>
2026-08-19 13:41:20 +0300

    Update rewardEngine.js

--- js/services/rewardEngine.js
+++ js/services/rewardEngine.js
@@ -1,1 +1,19 @@
 
+const RewardEngine = {
+    // معالجة الأحداث وتوزيع الإنجازات
+    processEvent(eventType, payload) {
+        const data = Store.get();
+
+        if (eventType === 'WRITE_WORDS') {
+            data.user.stats.wordsWritten += payload.count || 0;
+        } else if (eventType === 'READ_BOOK') {
+            data.user.stats.booksRead += payload.count || 1;
+        } else if (eventType === 'JOIN_EVENT') {
+            data.user.stats.eventsJoined += payload.count || 1;
+        }
+
+        Store.save(data);
+        BadgeService.checkBadges();
+        StreakService.recordActivity();
+    }
+};
