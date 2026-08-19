54f25cbda942481bcf5405b7cc2a824ab95b81cd
doha-almedad <ll4205858@gmail.com>
2026-08-19 13:42:00 +0300

    Update badgeService.js

--- js/services/badgeService.js
+++ js/services/badgeService.js
@@ -1,1 +1,21 @@
 
+const BadgeService = {
+    checkBadges() {
+        const data = Store.get();
+        let updated = false;
+
+        data.badges.forEach(badge => {
+            if (!badge.unlocked) {
+                // شروط فتح الأوسمة
+                if (badge.id === 'b3' && data.user.stats.booksRead >= 3) {
+                    badge.unlocked = true;
+                    updated = true;
+                }
+            }
+        });
+
+        if (updated) {
+            Store.save(data);
+        }
+    }
+};
