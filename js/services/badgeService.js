b2c9b422e4e24d78871c4d981494fe8f560fe898
doha-almedad <ll4205858@gmail.com>
2026-09-04 23:20:25 +0300

    Add files via upload

--- js/services/badgeService.js
+++ js/services/badgeService.js
@@ -8,7 +8,7 @@
 import { BADGE_DEFINITIONS } from "../db/initialData.js";
 import { store } from "../db/store.js";
 
-function allDefinitions(){ const overrides=store.getBadgeOverrides();return [...BADGE_DEFINITIONS.map(def=>({...def,...(overrides[def.id]||{}),literaryDesc:{...def.literaryDesc,...(overrides[def.id]?.literaryDesc||{})}})), ...store.getCustomBadges()]; }
+function allDefinitions(){ const overrides=store.getBadgeOverrides(),disabled=new Set(store.getDisabledBadges());return [...BADGE_DEFINITIONS.filter(def=>!disabled.has(def.id)).map(def=>({...def,...(overrides[def.id]||{}),literaryDesc:{...def.literaryDesc,...(overrides[def.id]?.literaryDesc||{})}})), ...store.getCustomBadges()]; }
 
 function currentValueFor(user, conditionType){
   switch(conditionType){
